13b7678c63b7d16d9ef0360c23f0ee68a8f0f2fd
Mikhail Priver <m.priver@gmail.com>
2026-08-06 19:18:28 +0300

    Update GitHub Actions (#74)

--- packages/eslint-config/scripts/compare.ts
+++ packages/eslint-config/scripts/compare.ts
@@ -329,10 +329,10 @@ function comparePlugin(
   }
 
   if (incompatibleTable.length > 0) {
-    markdownLines.push(htmlTable(incompatibleTable));
+    markdownLines.push('', htmlTable(incompatibleTable));
   }
 
-  markdownLines.push(links.join('\n'));
+  markdownLines.push('', ...links);
 
   const start = htmlCommentStart(pluginPrefix);
   const end = htmlCommentEnd(pluginPrefix);
